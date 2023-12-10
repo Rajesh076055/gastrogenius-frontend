8f7d1836d431ec3a98c6a34036e6187e8c92d9de
jharahul968 <jharahul968@gmail.com>
2023-12-11 00:29:22 +0545

    change socket io to websocket and make frontend work with fastapi

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { TextField, Input, Select,MenuItem, FormControl, InputLabel, Button } from '@mui/material'
+import { TextField, Input, Select, MenuItem, FormControl, InputLabel, Button } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
@@ -11,23 +11,23 @@ import Box from '@mui/material/Box';
 
 const useStyles = makeStyles(() => ({
   inputField: {
-    width: '30%', 
+    width: '30%',
   },
 }));
 
 const style = {
-  root:{
-    backgroundColor:"#92400E",
-    marginTop:4,
-    ':hover':{
-      backgroundColor:"#92400E",
-      boxShadow:"default"
+  root: {
+    backgroundColor: "#92400E",
+    marginTop: 4,
+    ':hover': {
+      backgroundColor: "#92400E",
+      boxShadow: "default"
     },
-    boxShadow:'none',
+    boxShadow: 'none',
   }
- }
+}
 
- const styleBox = {
+const styleBox = {
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -39,16 +39,10 @@ const style = {
   p: 0,
 };
 
- const socket = io('http://localhost:5000');
-
- socket.on('connect', () => {
-  console.log('Connected to server');
-});
-
 function Register() {
 
   const canvasRef = React.useRef(null);
-  const diagnosisTypes = ["Endoscopy","Colonoscopy"];
+  const diagnosisTypes = ["Endoscopy", "Colonoscopy"];
   const [diagnosis, setDiagnosis] = React.useState('');
   const [name, setName] = React.useState('');
   const [open, setOpen] = React.useState(false);
@@ -56,6 +50,7 @@ function Register() {
   const [filename, setFilename] = React.useState('');
   const [currentFrameIndex, setCurrentFrameIndex] = React.useState(0);
   const [goodToStart, setGoodToStart] = React.useState(false);
+  const [socket, setSocket] = React.useState(null);
 
   const handleChange = (e) => {
     setDiagnosis(e.target.value);
@@ -67,57 +62,75 @@ function Register() {
   }
 
 
-  const handleVideoInput = async(e)=> {
-    
+  const handleVideoInput = async (e) => {
+
     const videoFile = e.target.files[0];
 
     const formData = new FormData();
-    formData.append("file",videoFile);
+    formData.append("file", videoFile);
 
 
     try {
-      const response = await axios.post("http://localhost:5000/send-videos",formData, {
-        headers: {'Content-Type':'multipart/form-data'},
+      const response = await axios.post("http://localhost:8000/send-videos", formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
       });
 
 
-      if (response.data.ack)
-      {
+      if (response.data.ack) {
         setGoodToStart(true);
       }
 
       setGoodToGo(response.data.ack);
       setFilename(response.data.filepath);
 
-      // console.log("We have received a message from flask app: ",response.data.ack);
+      console.log("We have received acknowledgement from fastapi app: ", response.data.ack);
+      console.log("We have received filepath from fastapi app: ", response.data.filepath);
 
     } catch (error) {
-      console.log("Error uploading video!",error);
-    }   
-  }
-
-  const startSession =()=> {
-    
-    setOpen(true);
-    socket.emit("frame",filename);
-
-    return ()=> {
-      socket.off('frame');
+      console.log("Error uploading video!", error);
     }
   }
 
-  useEffect(()=>{
+  const startSession = () => {
+    setOpen(true);
+    if (!socket) {
+      const newSocket = new WebSocket('ws://localhost:8000/ws');
+      setSocket(newSocket);
 
-    socket.on("extracted-frame",(frame)=>{
+      newSocket.onopen = function (event) {
+        console.log("WebSocket connected");
+        newSocket.send(filename);
+      };
+      console.log("going to return");
 
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-  
-      const renderFrames = async() => {
-      
+      return () => {
+        newSocket.close();
+        console.log("returned");
+      };
+    }
+  };
+  useEffect(() => {
+
+    console.log("trying to enter");
+    if (socket) {
+      console.log("entered");
+      socket.onmessage = function (event) {
+        const frame = event.data;
+
+        console.log(typeof (frame))
+
+
+        const canvas = canvasRef.current;
+        console.log('canvas: ', typeof(canvas))
+        if (canvas) {
+          console.log("entered into canvas")
+          const ctx = canvas.getContext('2d');
+
+          // const renderFrames = async() => {
+
           const img = new Image();
           img.src = `data:image/jpeg;base64,${frame}`;
-  
+          console.log(typeof(img))
           img.onload = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
@@ -125,42 +138,42 @@ function Register() {
             //requestAnimationFrame(renderFrames);
           };
         }
-  
-        
-      
-  
-      
-      renderFrames();
+      }
 
-      // callback('Frame received');
-    
-    });
-   
-    return ()=> {
-      socket.off("extracted-frame");
-      //worker.current.terminate();
+
+
+
+      // renderFrames();    
+      // }
     }
 
-  },[]);
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
 
 
- 
+  }, [socket]);
+
+
+
 
 
   const classes = useStyles();
   return (
-    <div className = "flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className='mb-10 font-semibold text-4xl'>Please enter the details</h1>
-      <TextField  id="outlined-basic"
-        label="Your Full Name" 
-        variant="outlined" 
-        className={classes.inputField} 
+      <TextField id="outlined-basic"
+        label="Your Full Name"
+        variant="outlined"
+        className={classes.inputField}
         InputProps={{
           inputProps: {
             autoCapitalize: 'words',
           },
-          }}
-        onChange={(e)=>{setName(e.target.value)}}
+        }}
+        onChange={(e) => { setName(e.target.value) }}
       />
 
       <FormControl className={`${classes.inputField} mt-6`}>
@@ -178,30 +191,30 @@ function Register() {
           <MenuItem value={diagnosisTypes[1]}>Colonoscopy</MenuItem>
 
         </Select>
-    </FormControl>
+      </FormControl>
 
-    
 
-    <Input type = "file" accept="video/*" onChange = {handleVideoInput} sx = {{marginTop:3}} ></Input>
-    <FormGroup className='mt-7'>
-      <FormControlLabel control={<Checkbox disabled={name?(diagnosis?false:true):true}/>} label="I want the system to save the footages of findings" />
-    </FormGroup>
-    <Button sx = {style.root} className={`${classes.inputField}`} disabled = {!goodToStart} variant = "contained" onClick={startSession}>Start the session</Button>
 
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      
-      <Box sx={styleBox}>
-        <canvas ref={canvasRef} width={540} height={280}></canvas>
-      </Box>
-      
-    </Modal>
-   
-    
+      <Input type="file" accept="video/*,image/*" onChange={handleVideoInput} sx={{ marginTop: 3 }} ></Input>
+      <FormGroup className='mt-7'>
+        <FormControlLabel control={<Checkbox disabled={name ? (diagnosis ? false : true) : true} />} label="I want the system to save the footages of findings" />
+      </FormGroup>
+      <Button sx={style.root} className={`${classes.inputField}`} disabled={!goodToStart} variant="contained" onClick={startSession}>Start the session</Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+
+        <Box sx={styleBox}>
+          <canvas ref={canvasRef} width={540} height={280}></canvas>
+        </Box>
+
+      </Modal>
+
+
     </div>
   )
 }
